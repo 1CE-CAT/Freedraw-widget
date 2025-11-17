dd99c26a7edd96eab2a58aa49e6dfb317ab12f2c
1CE-CAT <gisnet9@hotmail.com>
2025-11-17 22:47:44 +0500

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,121 @@
+import {
+  ReactSketchCanvas,
+  type ReactSketchCanvasRef,
+} from "react-sketch-canvas";
+import { type ChangeEvent, useRef, useState } from "react";
+
+export default function App() {
+  const canvasRef = useRef<ReactSketchCanvasRef>(null);
+  const [eraseMode, setEraseMode] = useState(false);
+  const [strokeWidth, setStrokeWidth] = useState(5);
+  const [eraserWidth, setEraserWidth] = useState(10);
+  const [strokeColor, setStrokeColor] = useState("#000000");
+  const [canvasColor, setCanvasColor] = useState("#ffffff");
+
+  const handleEraserClick = () => {
+    setEraseMode(true);
+    canvasRef.current?.eraseMode(true);
+  };
+
+  const handlePenClick = () => {
+    setEraseMode(false);
+    canvasRef.current?.eraseMode(false);
+  };
+
+  const handleStrokeWidthChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setStrokeWidth(+event.target.value);
+  };
+
+  const handleEraserWidthChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setEraserWidth(+event.target.value);
+  };
+
+  const handleStrokeColorChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setStrokeColor(event.target.value);
+  };
+
+  const handleCanvasColorChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setCanvasColor(event.target.value);
+  };
+
+  const handleClearCanvas = () => {
+    if (canvasRef.current) {
+      canvasRef.current.clearCanvas();
+    }
+  };
+
+  return (
+    <div className="d-flex flex-column gap-2 p-2">
+      <h1>Tools</h1>
+      <div className="d-flex gap-2 align-items-center ">
+        <label htmlFor="color">Stroke color</label>
+        <input
+          type="color"
+          value={strokeColor}
+          onChange={handleStrokeColorChange}
+        />
+        <label htmlFor="color">Canvas color</label>
+        <input
+          type="color"
+          value={canvasColor}
+          onChange={handleCanvasColorChange}
+        />
+        <button
+          type="button"
+          className="btn btn-sm btn-outline-primary"
+          disabled={!eraseMode}
+          onClick={handlePenClick}
+        >
+          Pen
+        </button>
+        <button
+          type="button"
+          className="btn btn-sm btn-outline-primary"
+          disabled={eraseMode}
+          onClick={handleEraserClick}
+        >
+          Eraser
+        </button>
+        <label htmlFor="strokeWidth" className="form-label">
+          Stroke width
+        </label>
+        <input
+          disabled={eraseMode}
+          type="range"
+          className="form-range"
+          min="1"
+          max="20"
+          step="1"
+          id="strokeWidth"
+          value={strokeWidth}
+          onChange={handleStrokeWidthChange}
+        />
+        <label htmlFor="eraserWidth" className="form-label">
+          Eraser width
+        </label>
+        <input
+          disabled={!eraseMode}
+          type="range"
+          className="form-range"
+          min="1"
+          max="20"
+          step="1"
+          id="eraserWidth"
+          value={eraserWidth}
+          onChange={handleEraserWidthChange}
+        />
+      </div>
+      <button onClick={handleClearCanvas}>
+        Очистить canvas
+      </button>
+      <h1>Canvas</h1>
+      <ReactSketchCanvas
+        ref={canvasRef}
+        strokeColor={strokeColor}
+        canvasColor={canvasColor}
+        eraserWidth={eraserWidth}
+        strokeWidth={strokeWidth}
+      />
+    </div>
+  );
+}
